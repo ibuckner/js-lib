9e2924e40a8877122766dc7ecafd121848f1f47d
ibuckner <ibuckner@work>
2019-08-06 17:08:21 +0100

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import { unique } from "./array";
-import { aspectRatio, getJSON, pageOffset, normalizeWheel, toNodes } from "./dom";
+import {
+  aspectRatio, getJSON, pageOffset, normalizeWheel, toNodes } from "./dom";
 import { all, one } from "./dom-selection";
 import { Drag } from "./drag";
 import { circleIconCanvas, personIconCanvas } from "./icon-canvas";
-import { circleIcon, closeIcon, crossIcon, diamondIcon, ellipseIcon, exportIcon, legendIcon, magnifyIcon, rectangleIcon, selectIcon, squareIcon, triangleIcon, undoIcon } from "./icon-svg";
-import { isBrowser, isDate, isEmpty, isIE, isNumeric, isPostcode, yyyymmdd } from "./is";
-import { circleArea, circleRadius, countDigit, cubedRoot, fitToQuadrant, rectContain, rectIntersect, round, splitCurve, stddev, sumValues, triangleArea, triangleHeight } from "./math";
+import {
+  circleIcon, closeIcon, crossIcon, diamondIcon, ellipseIcon,
+  exportIcon, legendIcon, magnifyIcon, rectangleIcon, selectIcon,
+  squareIcon, triangleIcon, undoIcon } from "./icon-svg";
+import {
+  isBrowser, isDate, isEmpty, isIE, isNumeric, isPostcode, yyyymmdd
+} from "./is";
+import {
+  circleArea, circleRadius, countDigit, cubedRoot, fitToQuadrant,
+  rectContain, rectIntersect, round, splitCurve, stddev, sumValues,
+  triangleArea, triangleHeight } from "./math";
 import { Queue } from "./queue";
-import { rangeContiguous, rangeEmpty, selectionTrim, selectionTrimLeft, selectionTrimRight } from "./selection";
-import { getTranslation, pathArc, pathLine, pattern, textWidth } from "./svg-element";
+import {
+  rangeContiguous, rangeEmpty, selectionTrim, selectionTrimLeft,
+  selectionTrimRight } from "./selection";
+import {
+  getTranslation, pathArc, pathLine, pattern, textWidth
+} from "./svg-element";
 import { TPoint, TTextWidth } from "./types";
 
 export {
@@ -17,12 +30,17 @@ export {
   all, one,
   Drag,
   circleIconCanvas, personIconCanvas,
-  circleIcon, closeIcon, crossIcon, diamondIcon, ellipseIcon, exportIcon, legendIcon, magnifyIcon, rectangleIcon, selectIcon, squareIcon, triangleIcon, undoIcon,
+  circleIcon, closeIcon, crossIcon, diamondIcon, ellipseIcon,
+  exportIcon,
+  legendIcon, magnifyIcon, rectangleIcon, selectIcon, squareIcon,
+  triangleIcon, undoIcon,
   isBrowser, isDate, isEmpty, isIE, isNumeric, isPostcode, yyyymmdd,
-  circleArea, circleRadius, countDigit, cubedRoot, fitToQuadrant, rectContain, rectIntersect, round, splitCurve, 
+  circleArea, circleRadius, countDigit, cubedRoot, fitToQuadrant,
+  rectContain, rectIntersect, round, splitCurve,
   stddev, sumValues, triangleArea, triangleHeight,
   Queue,
-  rangeContiguous, rangeEmpty, selectionTrim, selectionTrimLeft, selectionTrimRight,
-  getTranslation, pathArc, pathLine, pattern, textWidth, 
+  rangeContiguous, rangeEmpty, selectionTrim, selectionTrimLeft,
+  selectionTrimRight,
+  getTranslation, pathArc, pathLine, pattern, textWidth,
   TPoint, TTextWidth
 };
