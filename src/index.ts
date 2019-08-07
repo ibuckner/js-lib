801f7d23eaee527e18cc60ea349ab66f4b43231e
ibuckner <ibuckner@work>
2019-08-07 10:25:56 +0100

    Add RGB to main package

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,29 @@
 import { unique } from "./array";
+import { RGB } from "./color";
 import {
-  aspectRatio, getJSON, pageOffset, normalizeWheel, toNodes } from "./dom";
+  aspectRatio, getJSON, pageOffset, normalizeWheel, toNodes
+} from "./dom";
 import { all, one } from "./dom-selection";
 import { Drag } from "./drag";
 import { circleIconCanvas, personIconCanvas } from "./icon-canvas";
 import {
   circleIcon, closeIcon, crossIcon, diamondIcon, ellipseIcon,
   exportIcon, legendIcon, magnifyIcon, rectangleIcon, selectIcon,
-  squareIcon, triangleIcon, undoIcon } from "./icon-svg";
+  squareIcon, triangleIcon, undoIcon
+} from "./icon-svg";
 import {
   isBrowser, isDate, isEmpty, isIE, isNumeric, isPostcode, yyyymmdd
 } from "./is";
 import {
   circleArea, circleRadius, countDigit, cubedRoot, fitToQuadrant,
   rectContain, rectIntersect, round, splitCurve, stddev, sumValues,
-  triangleArea, triangleHeight } from "./math";
+  triangleArea, triangleHeight
+} from "./math";
 import { Queue } from "./queue";
 import {
   rangeContiguous, rangeEmpty, selectionTrim, selectionTrimLeft,
-  selectionTrimRight } from "./selection";
+  selectionTrimRight
+} from "./selection";
 import {
   getTranslation, pathArc, pathLine, pattern, textWidth
 } from "./svg-element";
@@ -26,6 +31,7 @@ import { TPoint, TTextWidth } from "./types";
 
 export {
   unique,
+  RGB,
   aspectRatio, getJSON, pageOffset, normalizeWheel, toNodes,
   all, one,
   Drag,
