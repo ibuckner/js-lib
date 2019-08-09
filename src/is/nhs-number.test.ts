291dab6ae31b734bfd1d7146441386386fd07579
ibuckner <ibuckner@users.noreply.github.com>
2019-08-09 12:04:07 +0100

    Add isNHSNumber

--- src/is/nhs-number.test.ts
+++ src/is/nhs-number.test.ts
@@ -3,4 +3,7 @@ import { isNHSNumber } from "./nhs-number";
 test("isNHSNumber function tests", () => {
   expect(isNHSNumber("446 610 5715")).toBe(true);
   expect(isNHSNumber("446 610 1234")).toBe(false);
+  expect(isNHSNumber("401 023 2137")).toBe(true);
+  expect(isNHSNumber("9961234567")).toBe(false);
+  expect(isNHSNumber("1111111111")).toBe(false);
 });
