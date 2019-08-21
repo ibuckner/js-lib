061a9fb00759f09007bd4a23232e4ba934ef0b98
ibuckner <ibuckner@work>
2019-08-21 09:27:50 +0100

    Package update + test

---
+++ src/queue/queue.test.ts
@@ -0,0 +1,23 @@
+import { Queue } from "./index";
+
+test("Queue tests", () => {
+  const q1: Queue<any> = new Queue();
+  const q2: Queue<number> = new Queue([1,2,3,4,5]);
+  expect(q1.length).toStrictEqual(0);
+  expect(q2.length).toStrictEqual(5);
+  expect(q1.first).toBeNull();
+  expect(q2.first).toStrictEqual(1);
+  expect(q1.last).toBeNull();
+  expect(q2.last).toStrictEqual(5);
+  q2.clear();
+  expect(q2.length).toStrictEqual(0);
+  q2.join(1).join(2).join(3).jump(11).join(33);
+  expect(q2.length).toStrictEqual(5);
+  expect(q2.first).toStrictEqual(11);
+  expect(q2.last).toStrictEqual(33);
+  const a: number = q2.leave();
+  const b: number = q2.next();
+  expect(a).toStrictEqual(33);
+  expect(b).toStrictEqual(11);
+  expect(q2.toArray()).toStrictEqual([1,2,3]);
+});
