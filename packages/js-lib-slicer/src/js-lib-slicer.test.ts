ed63d68f59cb4968bde54dd7be6b9632d3981291
ibuckner <ibuckner@work>
2020-02-06 16:59:23 +0000

    WIP

--- packages/js-lib-slicer/src/js-lib-slicer.test.ts
+++ packages/js-lib-slicer/src/js-lib-slicer.test.ts
@@ -1,61 +1,125 @@
-import { Slicer } from "./js-lib-slicer";
+import { Slicer, SlicerModifier } from "./js-lib-slicer";
 
-test("Queue tests", () => {
-  const slicer: Slicer<string> = new Slicer(["cat", "dog", "rat"]);
-  
-  slicer.toggle("rat");
-  expect(slicer.data.get("rat"))
-    .toStrictEqual({ filtered: false, selected: true });
-    expect(slicer.active).toBe(true);
+test("Test slicer using Ctrl key", () => {
+  const slicer: Slicer<string> = new Slicer<string>(["cat", "dog", "rat"]);
+  expect(slicer.selected).toBe(0);
+  expect(slicer.lastSelection).toBeUndefined();
 
-  slicer.clear();
-  expect(slicer.active).toBe(false);
-  expect(slicer.data.get("rat"))
-    .toStrictEqual({ filtered: false, selected: false });
+  slicer.toggle("rat", SlicerModifier.CTRL_KEY);
+  expect(slicer.data.get("rat")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.data.get("cat")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.selected).toBe(1);
+  expect(slicer.lastSelection).toBe("rat");
 
-  slicer.toggle("rat");
-  expect(slicer.data.get("cat"))
-    .toStrictEqual({ filtered: true, selected: false });
-  expect(slicer.data.get("rat"))
-    .toStrictEqual({ filtered: false, selected: true });
+  slicer.toggle("cat", SlicerModifier.CTRL_KEY);
+  expect(slicer.data.get("rat")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.data.get("cat")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.selected).toBe(2);
+  expect(slicer.lastSelection).toBe("cat");
 
-  slicer.toggle("cat", true);
-  expect(slicer.data.get("cat"))
-    .toStrictEqual({ filtered: false, selected: true });
-  expect(slicer.data.get("rat"))
-    .toStrictEqual({ filtered: false, selected: true });
-  expect(slicer.data.get("dog"))
-    .toStrictEqual({ filtered: true, selected: false });
+  slicer.toggle("rat", SlicerModifier.CTRL_KEY);
+  expect(slicer.data.get("rat")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.data.get("cat")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.selected).toBe(1);
+  expect(slicer.lastSelection).toBe("rat");
 
-  slicer.toggle("rat", true);
-  expect(slicer.data.get("cat"))
-    .toStrictEqual({ filtered: false, selected: true });
-  expect(slicer.data.get("rat"))
-    .toStrictEqual({ filtered: true, selected: false });
-  expect(slicer.data.get("dog"))
-    .toStrictEqual({ filtered: true, selected: false });
+  slicer.toggle("cat", SlicerModifier.CTRL_KEY);
+  expect(slicer.data.get("rat")).toStrictEqual({ filtered: false, selected: false });
+  expect(slicer.data.get("cat")).toStrictEqual({ filtered: false, selected: false });
+  expect(slicer.selected).toBe(0);
+  expect(slicer.lastSelection).toBeUndefined();
+});
 
-  slicer.toggle("cat", true);
-  expect(slicer.data.get("cat"))
-    .toStrictEqual({ filtered: false, selected: false });
-  expect(slicer.data.get("rat"))
-    .toStrictEqual({ filtered: false, selected: false });
-  expect(slicer.data.get("dog"))
-    .toStrictEqual({ filtered: false, selected: false });
+test("Test slicer with no key", () => {
+  const slicer: Slicer<string> = new Slicer<string>(["cat", "dog", "rat"]);
+  expect(slicer.selected).toBe(0);
+  expect(slicer.lastSelection).toBeUndefined();
 
   slicer.toggle("rat");
-  expect(slicer.data.get("rat"))
-    .toStrictEqual({ filtered: false, selected: true });
-  expect(slicer.data.get("cat"))
-    .toStrictEqual({ filtered: true, selected: false });
-  expect(slicer.data.get("dog"))
-    .toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.data.get("rat")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.data.get("cat")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.selected).toBe(1);
+  expect(slicer.lastSelection).toBe("rat");
 
-  slicer.toggle("rat");
-  expect(slicer.data.get("rat"))
-    .toStrictEqual({ filtered: false, selected: false });
-  expect(slicer.data.get("cat"))
-    .toStrictEqual({ filtered: false, selected: false });
-  expect(slicer.data.get("dog"))
-    .toStrictEqual({ filtered: false, selected: false });
+  slicer.toggle("cat");
+  expect(slicer.data.get("rat")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.data.get("cat")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.selected).toBe(1);
+  expect(slicer.lastSelection).toBe("cat");
+
+  slicer.toggle("cat");
+  expect(slicer.data.get("rat")).toStrictEqual({ filtered: false, selected: false });
+  expect(slicer.data.get("cat")).toStrictEqual({ filtered: false, selected: false });
+  expect(slicer.selected).toBe(0);
+  expect(slicer.lastSelection).toBeUndefined();
+});
+
+test("Test slicer using Shift key", () => {
+  const slicer: Slicer<string> = new Slicer<string>(["cat", "dog", "rat", "flea", "mouse"]);
+  expect(slicer.selected).toBe(0);
+  expect(slicer.lastSelection).toBeUndefined();
+
+  slicer.toggle("cat");
+  expect(slicer.data.get("cat")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.data.get("dog")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.data.get("rat")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.data.get("flea")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.data.get("mouse")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.selected).toBe(1);
+  expect(slicer.lastSelection).toBe("cat");
+
+  slicer.toggle("flea", SlicerModifier.SHIFT_KEY);  
+  expect(slicer.data.get("cat")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.data.get("dog")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.data.get("rat")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.data.get("flea")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.data.get("mouse")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.selected).toBe(4);
+  expect(slicer.lastSelection).toBe("flea");
+
+  slicer.toggle("dog", SlicerModifier.SHIFT_KEY);
+  expect(slicer.data.get("cat")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.data.get("dog")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.data.get("rat")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.data.get("flea")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.data.get("mouse")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.selected).toBe(2);
+  expect(slicer.lastSelection).toBe("dog");
+
+  slicer.toggle("cat", SlicerModifier.SHIFT_KEY);
+  expect(slicer.data.get("cat")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.data.get("dog")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.data.get("rat")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.data.get("flea")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.data.get("mouse")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.selected).toBe(1);
+  expect(slicer.lastSelection).toBe("cat");
+
+  slicer.toggle("cat", SlicerModifier.SHIFT_KEY);
+  expect(slicer.data.get("cat")).toStrictEqual({ filtered: false, selected: false });
+  expect(slicer.data.get("dog")).toStrictEqual({ filtered: false, selected: false });
+  expect(slicer.data.get("rat")).toStrictEqual({ filtered: false, selected: false });
+  expect(slicer.data.get("flea")).toStrictEqual({ filtered: false, selected: false });
+  expect(slicer.data.get("mouse")).toStrictEqual({ filtered: false, selected: false });
+
+  expect(slicer.selected).toBe(0);
+  expect(slicer.lastSelection).toBeUndefined();
+
+  slicer.toggle("flea");
+  expect(slicer.data.get("cat")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.data.get("dog")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.data.get("rat")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.data.get("flea")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.data.get("mouse")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.selected).toBe(1);
+  expect(slicer.lastSelection).toBe("flea");
+
+  slicer.toggle("dog");
+  expect(slicer.data.get("cat")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.data.get("dog")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.data.get("rat")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.data.get("flea")).toStrictEqual({ filtered: false, selected: true });
+  expect(slicer.data.get("mouse")).toStrictEqual({ filtered: true, selected: false });
+  expect(slicer.selected).toBe(3);
+  expect(slicer.lastSelection).toBe("dog");
 });
--- packages/js-lib-slicer/src/js-lib-slicer.ts
+++ packages/js-lib-slicer/src/js-lib-slicer.ts
@@ -3,14 +3,15 @@ export type TSlicerState = {
   selected: boolean
 };
 
+export enum SlicerModifier {
+  NO_KEY = 0,
+  CTRL_KEY = 1,
+  SHIFT_KEY = 2
+}
+
 export class Slicer<T> {
   private _: Map<T, TSlicerState> = new Map<T, TSlicerState>();
-  private _active: boolean = false;
-
-  public get active(): boolean {
-    return this._active;
-  }
-
+  
   public get data(): any {
     return this._;
   }
@@ -25,6 +26,9 @@ export class Slicer<T> {
     }
   }
 
+  public lastSelection: T | undefined;
+  public selected: number = 0;
+
   constructor(list?: T[]) {
     if (list) {
       this.data = list;
@@ -38,57 +42,120 @@ export class Slicer<T> {
     this._.forEach((_: TSlicerState, key: T) => {
       this._.set(key, { filtered: false, selected: false });
     });
-    this._active = false;
+    this.selected = 0;
+    this.lastSelection = undefined;
     return this;
   }
 
   /**
-   * Toggles the state of an item in the slicer
-   * @param item - item identifier
-   * @param ctrlKey - state of Ctrl Key 
+   * Updates the slicer state
+   * @param item - item selected by user
+   * @param modifier - was any modifying key pressed
    */
-  public toggle(item: T, ctrlKey: boolean = false): Slicer<T> {
-    this._.forEach((value: TSlicerState, key: T) => {
-      if (item !== key) {
-        value.selected = ctrlKey ? value.selected : false;
-        value.filtered = ctrlKey ? false : true;          
+  public toggle(item: T, modifier: SlicerModifier = SlicerModifier.NO_KEY): Slicer<T> {
+    if (modifier === SlicerModifier.SHIFT_KEY) {
+      return this.toggleRange(item);
+    } else if (modifier === SlicerModifier.CTRL_KEY) {
+      return this.toggleCumulative(item);
+    } else {
+      return this.toggleSingle(item);
+    }
+  }
+
+  /**
+   * Updates the slicer state using Ctrl key modifier
+   * @param item - item selected by user
+   */
+  public toggleCumulative(item: T): Slicer<T> {
+    const state: TSlicerState | undefined = this._.get(item);
+    if (state) {
+      state.selected = !state.selected;
+      if (state.selected) {
+        ++this.selected;
       } else {
-        if (value.selected) {
-          value.selected = false;
-          value.filtered = false;
-        } else {
-          value.selected = !value.selected;
-          value.filtered = !value.selected;
-        }
+        --this.selected;
       }
-      this._.set(key, value);
-    });
-
-    const selectionList: T[] = [];
-    let filtered: number = 0;
-    this._.forEach((value: TSlicerState, key: T) => {
-      if (value.selected) {
-        selectionList.push(key);
-      }
-      if (value.filtered) {
-        ++filtered;
-      }
-    });
-
-    this._active = selectionList.length > 0 ? true : false;
-    
-    if (filtered === this._.size || 
-        selectionList.length === 0 || 
-        selectionList.length === this._.size) {
-      // Everything was selected or deselected
+    }
+    if (this.selected < 1) {
       this.clear();
-    } else if (selectionList.length > 0 && filtered === 0) {
+    } else {
+      this._.forEach((value: TSlicerState) => value.filtered = !value.selected);
+      this.lastSelection = item;
+    }
+    return this;
+  }
+
+  /**
+   * Updates the slicer state using Shift key modifier
+   * @param item - item selected by user
+   */
+  public toggleRange(item: T): Slicer<T> {
+    if (item === this.lastSelection) {
+      this.clear();
+    } else {
+      this.lastSelection = item;
+      this.selected = 0;
+      let rangeStart: T | undefined;
+      let rangeEnd: T | undefined;      
       this._.forEach((value: TSlicerState, key: T) => {
-        value.filtered = !value.selected;  
-        this._.set(key, value);
+        if (item === key) {
+          if (value.selected) {
+            rangeStart = key;
+            rangeEnd = key;
+          } else {
+            value.filtered = false;
+            value.selected = true;
+          }
+          if (rangeStart === undefined) {
+            rangeStart = key;
+          } else if (rangeStart && rangeEnd === undefined) {
+            rangeEnd = key;
+          }
+        } else if (value.selected && rangeStart === undefined) {
+          rangeStart = key;
+        } else {
+          if (rangeStart && rangeEnd === undefined) {
+            if (item === key) {
+              rangeEnd = key;
+            }
+            value.filtered = false;
+            value.selected = true;
+          } else {
+            value.filtered = true;
+            value.selected = false;
+          }
+        }
+        if (value.selected) {
+          ++this.selected;
+        }
       });
     }
-    
+    return this;
+  }
+
+  /**
+   * Updates the slicer state without key modifier
+   * @param item - item selected by user
+   */
+  public toggleSingle(item: T): Slicer<T> {
+    const state: TSlicerState | undefined = this._.get(item);
+    if (state) {
+      if (state.selected) {
+        this.clear();
+      } else {
+        this._.forEach((value: TSlicerState, key: T) => {
+          if (item === key) {
+            value.selected = !value.selected;
+            value.filtered = !value.selected;
+          } else {
+            value.selected = false;
+            value.filtered = true;
+          }
+        });
+        this.selected = 1;
+        this.lastSelection = item;
+      }
+    }
     return this;
   }
 }
